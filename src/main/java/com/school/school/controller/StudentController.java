a6f929cb08d6e04706642b75aa66e233a81de043
tecris-unk <intakvinta@gmail.com>
2026-02-12 22:58:49 +0300

    feat : response entity, find student by email

--- src/main/java/com/school/school/controller/StudentController.java
+++ src/main/java/com/school/school/controller/StudentController.java
@@ -6,6 +6,7 @@ import com.school.school.service.dto.StudentDTO;
 import com.school.school.service.mapper.StudentMapper;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.NonNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,47 +14,72 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/students")
 @AllArgsConstructor
-public class StudentController {
+public final class StudentController {
 
-    private final StudentService studentService;
+    private final StudentService service;
 
-    private final StudentMapper studentMapper;
+    private final StudentMapper mapper;
 
     @GetMapping("/{id}")
-    public ResponseEntity<StudentDTO> findById(@PathVariable Long id) {
-        Student student = studentService.findStudentById(id);
+    public ResponseEntity<@NonNull StudentDTO> findById(
+            @PathVariable final Long id
+    ) {
+        Student student = service.findStudentById(id);
+        if (student == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(mapper.toDTO(student));
+    }
+
+    @GetMapping()
+    public ResponseEntity<@NonNull List<StudentDTO>> getAllStudents() {
+        List<Student> students = service.findAllStudents();
+        if (students.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        List<StudentDTO> dtoList = students.stream()
+                .map(mapper::toDTO)
+                .toList();
+        return new ResponseEntity<>(dtoList, HttpStatus.OK);
+    }
+
+    @GetMapping("/by-email")
+    public ResponseEntity<StudentDTO> findByEmail(
+            @RequestParam String email
+    ) {
+        Student student = service.findStudentByEmail(email);
         if (student == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
-        return ResponseEntity.ok(studentMapper.toDTO(student));
+        return ResponseEntity.ok(mapper.toDTO(student));
     }
-
-    @GetMapping()
-    public List<StudentDTO> getAllStudents() {
-        List<Student> allStudents = studentService.findAllStudents();
-        return allStudents.stream()
-                .map(studentMapper::toDTO)
-                .toList();
-    }
-
-    @GetMapping()
 
     @PostMapping
-    public StudentDTO addStudent(@RequestBody StudentDTO studentDTO) {
-        Student student = studentService.createStudent(studentDTO);
-        return studentMapper.toDTO(student);
+    public ResponseEntity<@NonNull StudentDTO> addStudent(@RequestBody final StudentDTO studentDTO) {
+        Student student = service.createStudent(studentDTO);
+        if (student == null) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        return ResponseEntity.ok(mapper.toDTO(student));
     }
 
     @PutMapping("/{id}")
-    public StudentDTO updateStudent(@PathVariable Long id, @RequestBody StudentDTO updatedStudent) {
-        Student student = studentService.updateStudent(id, updatedStudent);
-        return studentMapper.toDTO(student);
+    public ResponseEntity<@NonNull StudentDTO> updateStudent(
+            @PathVariable final Long id,
+            @RequestBody final StudentDTO updatedStudent) {
+        Student student = service.updateStudent(id, updatedStudent);
+        if (student == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(mapper.toDTO(student));
     }
 
     @DeleteMapping("/{id}")
-    public void deleteStudent(@PathVariable Long id) {
-        studentService.deleteStudent(id);
+    public ResponseEntity<Void> deleteStudent(@PathVariable final Long id) {
+        boolean isDeleted = service.deleteStudent(id);
+        if (!isDeleted) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.noContent().build();
     }
-
-
 }

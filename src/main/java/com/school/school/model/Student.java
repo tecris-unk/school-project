b7b44db86211def069f41e5e9a4307d8b505dd5b
tecris-unk <intakvinta@gmail.com>
2026-02-12 22:57:33 +0300

    feat : added columns and table

--- src/main/java/com/school/school/model/Student.java
+++ src/main/java/com/school/school/model/Student.java
@@ -1,33 +1,32 @@
 package com.school.school.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "students")
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "last_name")
     private String lastName;
 
+    @Column(nullable = false)
     private int grade;
 
     @Enumerated(EnumType.STRING)
     private Gender gender;
+
     public enum Gender {
         MALE, FEMALE
     }
 
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String email;
 }

26b7f14c8a2d51e2eb088c1f0411f686115e1d9d
tecris-unk <intakvinta@gmail.com>
2026-02-12 23:00:56 +0300

    feat : new func findByEmail

--- src/main/java/com/school/school/repository/StudentRepository.java
+++ src/main/java/com/school/school/repository/StudentRepository.java
@@ -1,8 +1,11 @@
 package com.school.school.repository;
 
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.school.school.model.Student;
 
-public interface StudentRepository extends JpaRepository<Student, Long> {
-    boolean existsByEmail(String email);
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<@NonNull Student, @NonNull Long> {
+    Optional<Student> findByEmail(String email);
 }

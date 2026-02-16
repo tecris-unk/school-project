5843053b965110ad16de81f5c6a92ab13c7ceae2
tecris-unk <intakvinta@gmail.com>
2026-02-16 11:22:28 +0300

    fixed service

--- src/main/java/com/school/school/service/StudentServiceImpl.java
+++ src/main/java/com/school/school/service/StudentServiceImpl.java
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 @Primary
-public final class StudentServiceImpl implements StudentService {
+public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository repository;
 

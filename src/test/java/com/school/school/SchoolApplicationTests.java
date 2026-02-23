96039092ef281ec9cccd7d3058d4ea48cceca32e
tecris-unk <intakvinta@gmail.com>
2026-02-23 18:30:32 +0300

    fixed test

--- src/test/java/com/school/school/SchoolApplicationTests.java
+++ src/test/java/com/school/school/SchoolApplicationTests.java
@@ -3,7 +3,11 @@ package com.school.school;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE,
+        properties = {
+                "springdoc.api-docs.enabled=false",
+                "springdoc.swagger-ui.enabled=false"
+        })
 class SchoolApplicationTests {
 
     @Test

eae5d460e4942b261bf476e4e9e8cf6a8648ea11
tecris-unk <intakvinta@gmail.com>
2026-02-25 21:00:59 +0300

    fixed some bugs

--- src/main/java/com/school/school/repository/StudentRepository.java
+++ src/main/java/com/school/school/repository/StudentRepository.java
@@ -1,9 +1,7 @@
 package com.school.school.repository;
 
 import com.school.school.model.Student;
-import java.util.List;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {

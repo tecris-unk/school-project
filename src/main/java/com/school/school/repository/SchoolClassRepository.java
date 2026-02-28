6e2c501d8bcb8ef88ab76b9f5b962344aa315887
tecris-unk <intakvinta@gmail.com>
2026-02-28 22:47:54 +0300

    added transactional and n+1 problem demonstrating

--- src/main/java/com/school/school/repository/SchoolClassRepository.java
+++ src/main/java/com/school/school/repository/SchoolClassRepository.java
@@ -2,7 +2,6 @@ package com.school.school.repository;
 
 import com.school.school.model.SchoolClass;
 import java.util.List;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SchoolClassRepository extends JpaRepository<SchoolClass, Long> {
